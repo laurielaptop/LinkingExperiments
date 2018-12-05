923f038e7bb6867c18b60aff1f37ee5b59baad0f
Laurie Laptop <laurie.laptop@gmail.com>
2018-12-05 14:09:42 +0000

    Add external Swift static framework

---
+++ TheExternalSwiftStaticFramework/TheExternalSwiftStaticFramework/TheExternalSwiftStaticFramework.h
@@ -0,0 +1,19 @@
+//
+//  TheExternalSwiftStaticFramework.h
+//  TheExternalSwiftStaticFramework
+//
+//  Created by Laurie Keith on 05/12/2018.
+//  Copyright © 2018 Laurie Laptop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TheExternalSwiftStaticFramework.
+FOUNDATION_EXPORT double TheExternalSwiftStaticFrameworkVersionNumber;
+
+//! Project version string for TheExternalSwiftStaticFramework.
+FOUNDATION_EXPORT const unsigned char TheExternalSwiftStaticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheExternalSwiftStaticFramework/PublicHeader.h>
+
+
